e99d6d4632ab1cb6084b500abd2f0f78a0e59ec3
Aliaksandr <am@mediacube.io>
2025-11-08 09:46:58 +0700

    init: init vite

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
